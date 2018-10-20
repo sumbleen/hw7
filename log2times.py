ba232e5a586deb61e7c2c7a22902da91141d7aa2
sumbleen <42788130+sumbleen@users.noreply.github.com>
2018-10-20 18:12:10 -0400

    Add files via upload
    scripts

--- log2times.py
+++ log2times.py
@@ -5,6 +5,12 @@ import numpy as np
 # read the log file
 df = pd.read_table('/scratch/psyc5171/dataset1/task-fingerfootlips_events.tsv')
 # adjust the times
+# df['Time'] = df['Time'] - df.iloc[0]['Duration'] - df.iloc[0]['Time']
+
+# remove non condition codes
+# df = df[~df['Code'].isin(['fix', 'rest', '4', '8', 'checker_flash', 'wait'])]
+# convert to seconds
+# df['Time'] = df['Time']/10000.0
 
 groups = df.groupby(df['trial_type'])
 times = groups['onset'].unique()
